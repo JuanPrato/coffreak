4a6193f37b3adddfd1cea471c4d367e26404a276
JuanPrato <pratojuanmanuel2@gmail.com>
2022-10-20 14:06:00 -0300

    faq skeleton component

---
+++ src/components/home/FAQ.tsx
@@ -0,0 +1,28 @@
+import { Title, Text } from "@mantine/core";
+
+export default function FAQ() {
+
+    return (
+        <section>
+            <Title order={3} align="center" >
+                ¿Cuanto dinero puedo ganar?
+            </Title>
+            <Text className="my-4">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem ullam aspernatur numquam ad eaque soluta, eum asperiores minima sapiente! Laudantium sunt expedita beatae sint est odio iure. Accusantium, blanditiis quasi.
+            </Text>
+            <Title order={3} align="center" >
+                ¿Qué es coffreak?
+            </Title>
+            <Text className="my-4">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem ullam aspernatur numquam ad eaque soluta, eum asperiores minima sapiente! Laudantium sunt expedita beatae sint est odio iure. Accusantium, blanditiis quasi.
+            </Text>
+            <Title order={3} align="center" >
+                ¿Porqué usarlo?
+            </Title>
+            <Text className="my-4">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem ullam aspernatur numquam ad eaque soluta, eum asperiores minima sapiente! Laudantium sunt expedita beatae sint est odio iure. Accusantium, blanditiis quasi.
+            </Text>
+        </section>
+    )
+
+}
