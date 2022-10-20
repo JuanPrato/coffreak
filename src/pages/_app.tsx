4fc2fed88a19260df48fccc5c88564557144cd81
JuanPrato <pratojuanmanuel2@gmail.com>
2022-10-20 14:05:30 -0300

    set up dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,39 @@
-// src/pages/_app.tsx
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
+import { Header } from "../components/common/Header";
+import Head from "next/head";
+import { createEmotionCache, MantineProvider } from "@mantine/core";
+
+const mantineCache = createEmotionCache({ key: "mantine", prepend: false });
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>COFFREAK</title>
+        <meta name="description" content="App to post content related with anime, manga, etc." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SessionProvider session={session}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          emotionCache={mantineCache}
+          theme={{colorScheme: "dark"}}
+        >
+          <div className="w-screen h-screen overflow-y-auto overflow-x-hidden">
+            <Header />
+            <Component {...pageProps} />
+          </div>
+        </MantineProvider>
+      </SessionProvider>
+    </>
   );
 };
 
